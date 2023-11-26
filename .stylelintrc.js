9d2c8821f9d1000eb6f226029f2418f6db1d3dbe
Chopper <grinpis01@gmail.com>
2023-11-26 04:46:01 +0200

    newLinters

---
+++ .stylelintrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+    extends: ['stylelint-config-standard', 'stylelint-config-rational-order', 'stylelint-prettier/recommended'],
+    plugins: ['stylelint-order'],
+    rules: {
+        // Дополнительные правила
+    },
+};
