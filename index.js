1dc95436f5c73c40dcd0c71a855b90cc8e70526c
Chopper <grinpis01@gmail.com>
2023-11-27 15:06:25 +0200

    new

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-import { renderStartForm } from './modules/selectLvlGameForm.js';
-import { getElement } from './modules/getElementById.js';
+import { renderStartForm } from "./modules/selectLvlGameForm.js";
+import { getElement } from "./modules/getElementById.js";
 
+getElement().appElement ? console.log("live") : console.log("none");
 renderStartForm({ appEl: getElement().appElement });
