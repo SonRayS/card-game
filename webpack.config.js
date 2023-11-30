280abddb6d53c7bbf38a16ce48944a1e05347c84
Chopper <grinpis01@gmail.com>
2023-11-30 02:29:12 +0200

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,7 @@ module.exports = {
         rules: [
             {
                 test: /\.css$/,
-                use: [MiniCssExtractPlugin.loader, "css-loader"],
+                use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader"],
             },
             {
                 test: /\.(png|svg|jpg|jpeg|gif)$/i,
