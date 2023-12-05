0f815c041e837266970570434ee261109de9ae5a
Chopper <grinpis01@gmail.com>
2023-12-05 17:33:25 +0200

    new

---
+++ src/testCase.test.ts
@@ -0,0 +1,19 @@
+/* const firstCard = require("./testCase");
+const secundCard = require("./testCase");
+const cardsNumberArray = require("./testCase");
+
+describe("TEST CASE ONE", () => {
+    test("firstCard", () => {
+        expect(firstCard).toBeNull();
+    });
+
+    test("secundCard", () => {
+        expect(secundCard).toBeNull();
+    });
+
+    test("провекра элемента в массиве", () => {
+        expect(cardsNumberArray).toContain("Kk.png");
+        expect(new Set(cardsNumberArray)).toContain("Ac.png");
+    });
+});
+ */
