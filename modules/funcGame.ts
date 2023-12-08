21ed652032d6de61bfdb49290d72cfead1a13b4c
Chopper <grinpis01@gmail.com>
2023-12-09 00:12:02 +0200

    new

---
+++ modules/funcGame.ts
@@ -0,0 +1,129 @@
+import { getElement } from "./getElementById";
+import { renderLoseForm } from "./loseGameForm";
+import { renderWinForm } from "./winGameForm";
+import { renderStartForm } from "./selectLvlGameForm";
+
+let interval: NodeJS.Timeout;
+let minute = 0;
+let second = 0;
+let millisecond = 0;
+
+function startTimer() {
+    millisecond++;
+
+    if (millisecond > 99) {
+        second++;
+        getElement().secEl.innerHTML = "." + "0" + second;
+        millisecond = 0;
+    }
+
+    if (second <= 9) {
+        getElement().secEl.innerHTML = "." + "0" + second;
+    }
+    if (second > 9) {
+        getElement().secEl.innerHTML = "." + second;
+    }
+    if (second > 59) {
+        minute++;
+        getElement().minEl.innerHTML = "0" + minute;
+        second = 0;
+        getElement().secEl.innerHTML = "." + "0" + second;
+    }
+    if (minute > 9) {
+        getElement().minEl.innerHTML = "." + minute;
+    }
+}
+
+export function waitStartTime() {
+    clearInterval(interval);
+    interval = setInterval(startTimer, 10);
+}
+
+/* ______________________________________________________
+*
+*
+______________________________________________________ */
+
+export function startHide({ img, index }: { img: HTMLImageElement; index: number }) {
+    getElement().restartGame.addEventListener("click", () => {
+        clearInterval(interval);
+        renderStartForm({ appEl: getElement().appElement });
+    });
+    getElement().restartGame.style.background = "#7ac100";
+    img.setAttribute("src", "./img/hide.png");
+    img.classList.add("flip-scale-up-hor");
+    img.classList.add("cardHide");
+    img.setAttribute("id", "checkCards");
+
+    img.setAttribute("id", `${index}`);
+    getElement().ico.style.display = "none";
+}
+
+/* ______________________________________________________
+*
+*
+______________________________________________________ */
+
+export function startClick({
+    img,
+    firstCard,
+    secundCard,
+    appEl,
+    cardsArray,
+    src,
+}: {
+    img: HTMLImageElement;
+    firstCard: null | HTMLImageElement;
+    secundCard: null | HTMLImageElement;
+    appEl: HTMLElement;
+    cardsArray: { index: number; card: string }[];
+    src: string;
+}) {
+    img.addEventListener("click", function () {
+        img.src = src;
+
+        if (firstCard === null) {
+            firstCard = img;
+            console.log("firstCard", firstCard, firstCard.id);
+            firstCard.classList.add("flip-scale-up-hor");
+        } else if (secundCard === null) {
+            secundCard = img;
+            console.log("secundCard", secundCard, secundCard.id);
+            secundCard.classList.add("flip-scale-up-hor");
+            setTimeout(watchTimer, 400);
+        }
+
+        function watchTimer() {
+            if (firstCard !== null && secundCard !== null && firstCard.id !== secundCard.id) {
+                if (firstCard.src === secundCard.src) {
+                    console.log("Карточки совпали");
+                    firstCard.classList.add("hide");
+                    secundCard.classList.add("hide");
+                    firstCard = null;
+                    secundCard = null;
+                }
+            }
+
+            if (cardsArray.length === getElement().hides.length) {
+                setTimeout(function () {
+                    clearInterval(interval);
+                    renderWinForm({
+                        appEl,
+                        min: getElement().minEl.innerHTML,
+                        sec: getElement().secEl.innerHTML,
+                    });
+                }, 400);
+            }
+
+            if (firstCard !== null && secundCard !== null) {
+                clearInterval(interval);
+                console.log("Карточки не совпали");
+                renderLoseForm({
+                    appEl,
+                    min: getElement().minEl.innerHTML,
+                    sec: getElement().secEl.innerHTML,
+                });
+            }
+        }
+    });
+}
--- modules/mainGameForm.ts
+++ modules/mainGameForm.ts
@@ -1,7 +1,7 @@
 import { getElement } from "./getElementById";
-import { renderLoseForm } from "./loseGameForm";
-import { renderWinForm } from "./winGameForm";
-import { renderStartForm } from "./selectLvlGameForm";
+import { startHide } from "./funcGame";
+import { startClick } from "./funcGame";
+import { waitStartTime } from "./funcGame";
 
 export function formGameField({ appEl, lvl, pairs }: { appEl: HTMLElement; lvl: number; pairs: number }) {
     /* ------------------------------------------------- */
@@ -47,41 +47,6 @@ export function formGameField({ appEl, lvl, pairs }: { appEl: HTMLElement; lvl: 
     /* timer */
     /* ------------------------------------------------- */
 
-    let interval: NodeJS.Timeout;
-    let minute = 0;
-    let second = 0;
-    let millisecond = 0;
-
-    function startTimer() {
-        millisecond++;
-
-        if (millisecond > 99) {
-            second++;
-            getElement().secEl.innerHTML = "." + "0" + second;
-            millisecond = 0;
-        }
-
-        if (second <= 9) {
-            getElement().secEl.innerHTML = "." + "0" + second;
-        }
-        if (second > 9) {
-            getElement().secEl.innerHTML = "." + second;
-        }
-        if (second > 59) {
-            minute++;
-            getElement().minEl.innerHTML = "0" + minute;
-            second = 0;
-            getElement().secEl.innerHTML = "." + "0" + second;
-        }
-        if (minute > 9) {
-            getElement().minEl.innerHTML = "." + minute;
-        }
-    }
-
-    function waitStartTime() {
-        clearInterval(interval);
-        interval = setInterval(startTimer, 10);
-    }
     setTimeout(waitStartTime, 5000);
 
     /* ------------------------------------------------- */
@@ -149,86 +114,30 @@ export function formGameField({ appEl, lvl, pairs }: { appEl: HTMLElement; lvl: 
     }
 
     shuffle(cardsArray);
-    console.log(cardsArray);
 
     //create cards
 
     for (const i of cardsArray) {
-        const img = document.createElement("img");
+        let img = document.createElement("img");
         img.src = `./img/${i.card}`;
+        let arrayIndex: number = i.index;
+        console.log(arrayIndex);
 
-        // @ts-ignore: error message
-        function startHide() {
-            getElement().restartGame.addEventListener("click", () => {
-                clearInterval(interval);
-                renderStartForm({ appEl: getElement().appElement });
-            });
-            getElement().restartGame.style.background = "#7ac100";
-            img.setAttribute("src", "./img/hide.png");
-            img.classList.add("flip-scale-up-hor");
-            img.classList.add("cardHide");
-            img.setAttribute("id", "checkCards");
-
-            img.setAttribute("id", `${i.index}`);
-            getElement().ico.style.display = "none";
-        }
-
+        startHide({ img: img, index: arrayIndex });
         setTimeout(startHide, 5000);
+
         getElement().ico.style.display = "flex";
         getElement().restartGame.style.background = "red";
-
         getElement().game.append(img);
 
-        // @ts-ignore: error message
-        function startClick() {
-            img.addEventListener("click", function () {
-                img.src = `./img/${i.card}`;
-
-                if (firstCard === null) {
-                    firstCard = img;
-                    console.log("firstCard", firstCard, firstCard.id);
-                    firstCard.classList.add("flip-scale-up-hor");
-                } else if (secundCard === null) {
-                    secundCard = img;
-                    console.log("secundCard", secundCard, secundCard.id);
-                    secundCard.classList.add("flip-scale-up-hor");
-                    setTimeout(watchTimer, 400);
-                }
-
-                function watchTimer() {
-                    if (firstCard !== null && secundCard !== null && firstCard.id !== secundCard.id) {
-                        if (firstCard.src === secundCard.src) {
-                            console.log("Карточки совпали");
-                            firstCard.classList.add("hide");
-                            secundCard.classList.add("hide");
-                            firstCard = null;
-                            secundCard = null;
-                        }
-                    }
-
-                    if (cardsArray.length === getElement().hides.length) {
-                        setTimeout(function () {
-                            clearInterval(interval);
-                            renderWinForm({
-                                appEl,
-                                min: getElement().minEl.innerHTML,
-                                sec: getElement().secEl.innerHTML,
-                            });
-                        }, 400);
-                    }
-
-                    if (firstCard !== null && secundCard !== null) {
-                        clearInterval(interval);
-                        console.log("Карточки не совпали");
-                        renderLoseForm({
-                            appEl,
-                            min: getElement().minEl.innerHTML,
-                            sec: getElement().secEl.innerHTML,
-                        });
-                    }
-                }
-            });
-        }
-        setTimeout(startClick, 5000);
+        /*         startClick({
+            img: img,
+            firstCard: firstCard,
+            secundCard: secundCard,
+            appEl: appEl,
+            cardsArray: cardsArray,
+            src: img.src,
+        });
+        setTimeout(startClick, 5000); */
     }
 }
